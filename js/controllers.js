bd712a155c568265604080691489cfbaee7588c0
Bruno Pérez <bruno.perez@buddyweb.fr>
2016-01-16 20:14:54 +0100

    add loading spinner on addresses

--- js/controllers.js
+++ js/controllers.js
@@ -1,19 +1,19 @@
 app
     .controller('AddressesCtrl', ['$scope', 'MapRestService', '$window', '$state', 'AddressesService', function ($scope, MapRestService, $window, $state, AddressesService) {
 
-        $scope.goToAddMapForm = function(id) {
-            $state.go('addMap', {id: id});
-        };
+        // Show loading spinner.
+        $scope.loading = true;
 
         //// get list of items based on current location
         navigator.geolocation.getCurrentPosition(function (position) {
             MapRestService.getList(position.coords.latitude, position.coords.longitude)
 	            .then(function(addressesList){
 		            $scope.addresses = AddressesService.aggByAddress(addressesList.data);
-		            console.log($scope.addresses);
+                    $scope.loading = false;
 	            });
         }, function (error) {
             alert("impossible de vous localiser ");
+            $scope.loading = false;
             console.log(error);
         });
 
