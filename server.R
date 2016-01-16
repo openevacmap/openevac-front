7cf2c994f84547dd32aad1c3e01053e4243316bd
paul <paul.bouchequet@gmail.com>
2016-01-16 23:18:11 +0100

    v02

--- server.R
+++ server.R
@@ -4,33 +4,95 @@ library(rCharts)
 library(reshape2)
 library(httr)
 library(jpeg)
+library(rjson)
+library(stringr)
+library(digest)
 
-shinyServer(function(input, output) {
+queryResults <-
+  GET("http://api.openevacmap.org/v0/maps-info?lat=48.85&lon=2.33&nb_addr=0&nb_maps=999")
+content <-
+  as.character(content(queryResults, "text", encoding = "UTF-8"))
+content <- str_replace_all(content,"None","\"None\"")
+content <- fromJSON(content)
+content <- data.frame(t(sapply(content,c)))
+content$lat <- as.numeric(content$lat)
+content$lon <- as.numeric(content$lon)
+content <- content[content$id != "NULL",]
+
+content$id_bat <-
+  apply(content, 1, function(x) {
+    digest(x$address_label,algo = "md5")
+  })
+
+unique_content <- content[!duplicated(content$address_label),]
+
+
+shinyServer(function(input, output, session) {
+  # Points
+  points <- eventReactive(input$recalc, {
+    unique_content[c("lat","lon")]
+  }, ignoreNULL = FALSE)
   
   # Leaflet
   output$mymap <- renderLeaflet({
     leaflet() %>%
       addProviderTiles("OpenStreetMap.HOT",
-                       options = providerTileOptions(noWrap = TRUE)) %>% setView(lng = 2.3572111, lat = 48.8581874, zoom = 15) #%>% 
-    #addMarkers(data = list(2.3572111,48.8581874),layerId = 1)
+                       options = providerTileOptions(noWrap = TRUE)) %>% setView(lng = 2.3072111, lat = 48.8751874, zoom = 13) %>%
+      addMarkers(data = points(),layerId = unique_content$id_bat)
   })
   
   # Nom du batiment
-  output$name <- renderText({ 
-    "CRI Site Charles V"
+  output$name <- renderText({
+    "Sélectionnez un bâtiment"
   })
+  
+  observeEvent(input$mymap_marker_click, {
+    # http://stackoverflow.com/questions/33526256/dynamically-display-images-from-upload-in-shiny-ui
+    output$plan <- renderUI({
+      
+      
+      output$name<-renderText({})
+      output$address <- renderText({
+        content$address_label[content$id_bat == input$mymap_marker_click$id][[1]]
+      })
+      
+      images <- content[content$id_bat == input$mymap_marker_click$id,]
+      image_output_list <- 
+        apply(images, 1
+              ,
+               function(x)
+               {
+                 imgname <- paste("images/",x$id,".jpg", sep="")
+                 
+                 if(!file.exists(imgname)){
+                   url <- paste("http://api.openevacmap.org/v0/map?preview=1&id=",x$id,sep ="")[1]
+                   print(imgname)
+                   download.file(url,imgname)
+                 }
+                 imageOutput(imgname)
+               })
+      
+    do.call(tagList, image_output_list)
+    
 
-  # Images
-  output$plan <- renderImage({
     size <- 400
-    imgName <- "images/plan1.JPG"
-    img <- readJPEG(imgName)
-    list(
-      contentType = 'image/jpg',
-      width = size,
-      height = dim(img)[1]/dim(img)[2]*size,
-      src = "images/plan1.JPG",
-      alt = "Plan"
-    )
-  }, deleteFile = FALSE)
+    result <- apply(images, 1,
+          function(x)
+          {
+            local({
+              
+              imgname <- paste("images/",x$id,".jpg", sep="")
+              
+             img <- readJPEG(imgname)
+              output[[imgname]] <- renderImage({
+                list(src = imgname,alt = "Image failed to render", width = size,
+                     height = dim(img)[1] / dim(img)[2] * size)
+              }, deleteFile = FALSE)
+            })
+          })
+    
+    })
+  })
+  
+  
 })
--- ui.R
+++ ui.R
@@ -3,6 +3,22 @@ library(leaflet)
 library(rCharts)
 library(reshape2)
 
+queryResults <-
+  GET("http://api.openevacmap.org/v0/maps-info?lat=48.85&lon=2.33")
+content <-
+  as.character(content(queryResults, "text", encoding = "UTF-8"))
+content <- str_replace_all(content,"None","\"None\"")
+content <- fromJSON(content)
+content <- data.frame(t(sapply(content,c)))
+content$lat <- as.numeric(content$lat)
+content$lon <- as.numeric(content$lon)
+content$id_bat <-
+  apply(content, 1, function(x) {
+    digest(x$address_label,algo = "md5")
+  })
+
+unique_content <- content[!duplicated(content$address_label),]
+
 shinyUI(fluidPage(navbarPage("OpenEvacMap", id="map",
                              
                              tabPanel("Map",
@@ -15,11 +31,11 @@ shinyUI(fluidPage(navbarPage("OpenEvacMap", id="map",
                                       ),
                                       
                                       absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                                                    draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+                                                    draggable = TRUE, top = 60, left = 20, right = "auto", bottom = "auto",
                                                     width = "auto", height = "auto",
-                                                    
                                                     h3(textOutput("name"),align="center"),
-                                                    imageOutput("plan")
+                                                    textOutput("address"),align="center",
+                                                    uiOutput('plan')
                                       
                              )
                              
