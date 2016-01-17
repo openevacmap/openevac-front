6f5ec6a350ac57948aeda810a270b683b486fa8b
paul <paul.bouchequet@gmail.com>
2016-01-17 11:35:30 +0100

    v1

--- about.md
+++ about.md
@@ -1,1 +1,2 @@
-Map
+* Contribez en mettant à jour des plans d'évacuation sur [Openevacmap.org](http://openevacmap.org)
+* Retrouvez le code source du projet sur [Github.com/openevacmap/](https://github.com/openevacmap)
--- ui.R
+++ ui.R
@@ -3,22 +3,6 @@ library(leaflet)
 library(rCharts)
 library(reshape2)
 
-queryResults <-
-  GET("http://api.openevacmap.org/v0/maps-info?lat=48.85&lon=2.33")
-content <-
-  as.character(content(queryResults, "text", encoding = "UTF-8"))
-content <- str_replace_all(content,"None","\"None\"")
-content <- fromJSON(content)
-content <- data.frame(t(sapply(content,c)))
-content$lat <- as.numeric(content$lat)
-content$lon <- as.numeric(content$lon)
-content$id_bat <-
-  apply(content, 1, function(x) {
-    digest(x$address_label,algo = "md5")
-  })
-
-unique_content <- content[!duplicated(content$address_label),]
-
 shinyUI(fluidPage(navbarPage("OpenEvacMap", id="map",
                              
                              tabPanel("Map",
@@ -30,7 +14,7 @@ shinyUI(fluidPage(navbarPage("OpenEvacMap", id="map",
                                           leafletOutput("mymap", width="100%", height="100%")
                                       ),
                                       
-                                      absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                      absolutePanel(id = "controls", class = "panel panel-default", fixed = FALSE,
                                                     draggable = TRUE, top = 60, left = 20, right = "auto", bottom = "auto",
                                                     width = "auto", height = "auto",
                                                     h3(textOutput("name"),align="center"),
